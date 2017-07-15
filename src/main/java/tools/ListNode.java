206170add3707fedcfaa6d184f5b998db26d1c18
zhujia <izhujia@vip.qq.com>
2017-07-15 15:50:55 +0800

    添加链表节点类

---
+++ src/main/java/tools/ListNode.java
@@ -0,0 +1,13 @@
+package tools;
+
+/**
+ * Created by 24323 on 2017/7/15.
+ */
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
