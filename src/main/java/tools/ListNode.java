769befc96a88ef65e549210adc6cefb654848654
zhujia <izhujia@vip.qq.com>
2017-07-15 17:09:38 +0800

    修改节点类

--- src/main/java/tools/ListNode.java
+++ src/main/java/tools/ListNode.java
@@ -1,5 +1,6 @@
 package tools;
 
+
 /**
  * Created by 24323 on 2017/7/15.
  */
@@ -7,7 +8,7 @@ public class ListNode {
     public int val;
     public ListNode next = null;
 
-    ListNode(int val) {
+    public ListNode(int val) {
         this.val = val;
     }
 }
