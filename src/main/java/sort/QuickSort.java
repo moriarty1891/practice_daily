b67f4aaf9e3141ef0c50c8bc015083a97585712e
zhujia <izhujia@vip.qq.com>
2017-07-18 14:53:39 +0800

    增加注释

--- src/main/java/sort/QuickSort.java
+++ src/main/java/sort/QuickSort.java
@@ -4,6 +4,13 @@ package sort;
  * Created by zhujia on 2017/7/18.
  */
 //快速排序
+   /* 该方法的基本思想是：
+
+            1．先从数列中取出一个数作为基准数。
+
+            2．分区过程，将比这个数大的数全放到它的右边，小于或等于它的数全放到它的左边。
+
+            3．再对左右区间重复第二步，直到各区间只有一个数。*/
 public class QuickSort {
 
     public static int partition(int []array,int low,int high){
