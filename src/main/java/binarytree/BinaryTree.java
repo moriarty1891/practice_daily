363ed9c8004ca2b670bb4a40c7815d535c5ba253
zhujia <izhujia@vip.qq.com>
2017-07-20 14:56:38 +0800

    二叉树初始化

---
+++ src/main/java/binarytree/BinaryTree.java
@@ -0,0 +1,30 @@
+package binarytree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by zhujia on 2017/7/20.
+ */
+public class BinaryTree {
+    //初始化二叉树
+    public static List<TreeNode> initBinaryTree(int[] tree){
+        List<TreeNode> list = new ArrayList<>();
+        for (int treeNodeData : tree) {
+            list.add( new TreeNode(treeNodeData));
+        }
+        for(int parentNode = 0 ;parentNode< tree.length -1 ; parentNode++){
+            //初始化左儿子
+            list.get(parentNode).setLeftChild(list.get(parentNode*2 + 1));
+            //初始化右儿子
+            list.get(parentNode).setRightChild(list.get(parentNode*2 + 2));
+        }
+        //最后一个节点的度不一定是2，需要单独处理。
+        int lastNode = tree.length/2 -1;
+        list.get(lastNode).setLeftChild(list.get(lastNode*2+1));
+        if(tree.length%2!=0){
+            list.get(lastNode).setRightChild(list.get(lastNode*2+2));
+        }
+        return list;
+    }
+}
