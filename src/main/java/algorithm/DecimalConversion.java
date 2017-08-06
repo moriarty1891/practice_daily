7cdda3efef30b62ddec46499e22638ba84dd8d1b
zhujia <izhujia@vip.qq.com>
2017-08-06 13:56:02 +0800

    进制转换

---
+++ src/main/java/algorithm/DecimalConversion.java
@@ -0,0 +1,23 @@
+package algorithm;
+
+import java.util.Scanner;
+
+/**写出一个程序，接受一个十六进制的数值字符串，输出该数值的十进制字符串。（多组同时输入 ）
+
+ * Created by zhujia on 2017/8/6.
+ */
+public class DecimalConversion {
+    public static int calculation (String num){
+
+        return Integer.valueOf(num.substring(2),16);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()){
+            System.out.println(calculation(scanner.next()));
+        }
+
+
+    }
+}
