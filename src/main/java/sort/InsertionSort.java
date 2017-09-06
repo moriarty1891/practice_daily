c81fd60d713bbdd95cc1ab30b56d371ec7e05e69
zhujia <izhujia@vip.qq.com>
2017-09-06 15:38:58 +0800

    修改插入排序算法实现。

--- src/main/java/sort/InsertionSort.java
+++ src/main/java/sort/InsertionSort.java
@@ -1,34 +1,38 @@
 package sort;
 
 /**
+ *
  * Created by zhujia on 2017/7/10.
  */
 public class InsertionSort {
+    /**
+     * 插入排序。原地排序。
+     * @param a
+     */
+    public void InsertionSort(int[] a) {
+        int key,value;
+        //设数组中第一个元素有序。
+        for (int i = 1; i <a.length ; i++) {
+            value=a[i];
+            key=i;
+            //挨个向后移1个
+            while (a[key-1]>value&&key>=1){
+                a[key]=a[key-1];
+                key--;
+            }
+            //找到了合适的位置，塞进去。
+            a[key]=value;
+        }
+
+    }
+
+
     public static void main(String[] args) {
         int [] a = {49,38,65,97,76,13,27,49,78,34,12,64,1} ;
         System.out.println("before  sort：");
         for (int i = 0; i <a.length ; i++) {
             System.out.print(a[i] +" ");
         }
-
-
-
-        for(int i=1;i<a.length;i++){//从头部第一个当做已经排好序的，把后面的一个一个的插到已经排好的列表中去。
-            if(a[i]<a[i-1]){
-                int j;
-                int x=a[i];//x为待插入元素
-                a[i]=a[i-1];//直接后移一个，空出一个坑
-                for(j=i-1;  j>=0 && x<a[j];j--){//x需要小于比他大的元素。
-                    //通过循环，逐个后移一位找到要插入的位置。
-                    a[j+1]=a[j];
-                }
-                a[j+1]=x;//插入
-            }
-        }
-
-
-
-
             System.out.println();
         System.out.println("after sort：");
         for (int i = 0; i <a.length ; i++) {
