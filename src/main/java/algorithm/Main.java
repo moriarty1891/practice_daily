31eca9badd2fd207144c027dab0672a02f549f24
zhujia <izhujia@vip.qq.com>
2017-09-08 17:42:49 +0800

    Sogou

---
+++ src/main/java/algorithm/Main.java
@@ -0,0 +1,40 @@
+package algorithm;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Main {
+
+    public static String calculation(double[] array){
+        DecimalFormat df =new DecimalFormat("0.00000000");
+        double max=0;
+        double temp;
+        for (int i = 0; i <array.length ; i++) {
+            for (int j = 0; j <array.length ; j++) {
+                  if (array[i]>array[j]) temp = array[i]-array[j];
+                  else temp = array[j]-array[i];
+                  if (temp>180.00000000d){
+                      temp=360.00000000d-temp;
+                  }
+                  if (temp>max){
+                      max=temp;
+                  }
+
+            }
+        }
+        return df.format(max);
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()){
+            int number = scanner.nextInt();
+            double array[]= new double[number];
+            for (int i = 0; i <array.length ; i++) {
+                array[i]=scanner.nextDouble();
+            }
+            System.out.println(calculation(array));
+        }
+    }
+}
